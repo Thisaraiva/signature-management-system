354744687cca4a70092f16c2e912ea65dd3b6130
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2024-07-03 09:48:22 -0300

    Configuracao de dependencias de core e algumas configuracoes

--- Projeto/FrontEnd/pages/index.js
+++ Projeto/FrontEnd/pages/index.js
@@ -9,7 +9,7 @@ const Home = () => {
           <div className="w-full max-w-sm bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <h2 className="text-2xl text-center mb-4 font-bold">Welcome</h2>
             <div className="mb-4">
-              <Link href="/login">
+              <Link href="/login_materialui">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-2">
                   Login
                 </button>
